97c76f3baab4fefb1d065af50a748e76cd203cf8
Xiaqiong.Zhou <Xiaqiong.Zhou@noaa.gov>
2025-07-21 14:44:03 -0400

    1)revise archive jobs to only save atmos/master and ocn/ice history;2)remove the jobs dependences that doesnot exists such as atmos post for archive and cleanup jobs

--- dev/workflow/rocoto/sfs_tasks.py
+++ dev/workflow/rocoto/sfs_tasks.py
@@ -587,16 +587,20 @@ class SFSTasks(Tasks):
 
     def arch_tars(self):
         deps = []
-        dep_dict = {'type': 'metatask', 'name': f'{self.run}_atmos_prod'}
-        deps.append(rocoto.add_dependency(dep_dict))
-        dep_dict = {'type': 'metatask', 'name': f'{self.run}_atmos_ensstat'}
-        deps.append(rocoto.add_dependency(dep_dict))
-        if self.options['do_ice']:
-            dep_dict = {'type': 'metatask', 'name': f'{self.run}_ice_prod'}
+        for member in range(0, self.nmem + 1):
+            task = f'{self.run}_fcst_mem{member:03d}'
+            dep_dict = {'type': 'metatask', 'name': task}
             deps.append(rocoto.add_dependency(dep_dict))
-        if self.options['do_ocean']:
-            dep_dict = {'type': 'metatask', 'name': f'{self.run}_ocean_prod'}
-            deps.append(rocoto.add_dependency(dep_dict))
+#        dep_dict = {'type': 'metatask', 'name': f'{self.run}_atmos_prod'}
+#        deps.append(rocoto.add_dependency(dep_dict))
+#        dep_dict = {'type': 'metatask', 'name': f'{self.run}_atmos_ensstat'}
+#        deps.append(rocoto.add_dependency(dep_dict))
+#        if self.options['do_ice']:
+#            dep_dict = {'type': 'metatask', 'name': f'{self.run}_ice_prod'}
+#            deps.append(rocoto.add_dependency(dep_dict))
+#        if self.options['do_ocean']:
+#            dep_dict = {'type': 'metatask', 'name': f'{self.run}_ocean_prod'}
+#            deps.append(rocoto.add_dependency(dep_dict))
         if self.options['do_wave']:
             dep_dict = {'type': 'metatask', 'name': f'{self.run}_wave_post_grid'}
             deps.append(rocoto.add_dependency(dep_dict))
@@ -610,7 +614,8 @@ class SFSTasks(Tasks):
         if self.options['do_extractvars']:
             dep_dict = {'type': 'metatask', 'name': f'{self.run}_extractvars'}
             deps.append(rocoto.add_dependency(dep_dict))
-        dependencies = rocoto.create_dependency(dep=deps, dep_condition='and')
+        dependencies = rocoto.create_dependency(dep_condition='and', dep=deps)
+
 
         resources = self.get_resource('arch_tars')
         task_name = f'{self.run}_arch_tars'
@@ -631,16 +636,20 @@ class SFSTasks(Tasks):
 
     def cleanup(self):
         deps = []
-        dep_dict = {'type': 'metatask', 'name': f'{self.run}_atmos_prod'}
-        deps.append(rocoto.add_dependency(dep_dict))
-        dep_dict = {'type': 'metatask', 'name': f'{self.run}_atmos_ensstat'}
-        deps.append(rocoto.add_dependency(dep_dict))
-        if self.options['do_ice']:
-            dep_dict = {'type': 'metatask', 'name': f'{self.run}_ice_prod'}
+        for member in range(0, self.nmem + 1):
+            task = f'{self.run}_fcst_mem{member:03d}'
+            dep_dict = {'type': 'metatask', 'name': task}
             deps.append(rocoto.add_dependency(dep_dict))
-        if self.options['do_ocean']:
-            dep_dict = {'type': 'metatask', 'name': f'{self.run}_ocean_prod'}
-            deps.append(rocoto.add_dependency(dep_dict))
+#        dep_dict = {'type': 'metatask', 'name': f'{self.run}_atmos_prod'}
+#        deps.append(rocoto.add_dependency(dep_dict))
+#        dep_dict = {'type': 'metatask', 'name': f'{self.run}_atmos_ensstat'}
+#        deps.append(rocoto.add_dependency(dep_dict))
+#        if self.options['do_ice']:
+#            dep_dict = {'type': 'metatask', 'name': f'{self.run}_ice_prod'}
+#            deps.append(rocoto.add_dependency(dep_dict))
+#        if self.options['do_ocean']:
+#            dep_dict = {'type': 'metatask', 'name': f'{self.run}_ocean_prod'}
+#            deps.append(rocoto.add_dependency(dep_dict))
         if self.options['do_wave']:
             dep_dict = {'type': 'metatask', 'name': f'{self.run}_wave_post_grid'}
             deps.append(rocoto.add_dependency(dep_dict))
@@ -660,9 +669,9 @@ class SFSTasks(Tasks):
             else:
                 dep_dict = {'type': 'task', 'name': f'{self.run}_arch_tars'}
             deps.append(rocoto.add_dependency(dep_dict))
-            dependencies = rocoto.create_dependency(dep=deps)
-
         dependencies = rocoto.create_dependency(dep=deps, dep_condition='and')
+
+
         resources = self.get_resource('cleanup')
         task_name = f'{self.run}_cleanup'
         task_dict = {'task_name': task_name,
